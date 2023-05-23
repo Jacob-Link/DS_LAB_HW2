6f6094663f05c39e13609253adad7cba6769637e
Jacob-Link <jklklink@gmail.com>
2023-05-23 22:28:39 +0300

    fixed manually cleaned summary

--- data_loader.py
+++ data_loader.py
@@ -38,33 +38,45 @@ def print_counter(count_labels_dict, export=False):
 def print_counter_post_manual_clean(count_labels_dict, export=False):
     pre_manual_clean = []
     post_manual_clean = []  # called the new folder example: 1_out
-    total_labels = sum(count_labels_dict.values())
+
     for key, val in count_labels_dict.items():
-        tup = (key, val, f"{round(100 * val / total_labels, 2)}%")
+        tup = (key, val)
         if "_out" in key:
             post_manual_clean.append(tup)
         else:
             pre_manual_clean.append(tup)
 
-    pre_manual_clean.sort(key=lambda x: x[1], reverse=True)
-    post_manual_clean.sort(key=lambda x: x[1], reverse=True)
+    cleaned = []
+    total = sum(x[1] for x in pre_manual_clean)
+    for x in pre_manual_clean:
+        tup = (x[0], x[1],f"{round(100 * x[1] / total, 2)}%")
+        cleaned.append(tup)
 
-    print_df_pre = pd.DataFrame(pre_manual_clean, columns=["class", "count", "%"])
-    print_df_post = pd.DataFrame(post_manual_clean, columns=["class", "count", "%"])
+    removed = []
+    total = sum(x[1] for x in post_manual_clean)
+    for x in post_manual_clean:
+        tup = (x[0], x[1], f"{round(100 * x[1] / total, 2)}%")
+        removed.append(tup)
+
+    cleaned.sort(key=lambda x: x[1], reverse=True)
+    removed.sort(key=lambda x: x[1], reverse=True)
+
+    print_df_clean = pd.DataFrame(pre_manual_clean, columns=["class", "count", "%"])
+    print_df_removed = pd.DataFrame(post_manual_clean, columns=["class", "count", "%"])
     print("---------- manual clean ----------")
-    print(print_df_pre)
-    print(f"Size of clean train dataset: {sum(print_df_pre['count']):,}")
+    print(print_df_clean)
+    print(f"Size of clean train dataset: {sum(print_df_clean['count']):,}")
     print("--------------------------------------")
     print()
 
     print("---------- removed manual clean ----------")
-    print(print_df_post)
-    print(f"Size of removed: {sum(print_df_post['count']):,}")
+    print(print_df_removed)
+    print(f"Size of removed: {sum(print_df_removed['count']):,}")
     print("--------------------------------------")
 
     if export:
-        print_df_pre.to_csv("manual_clean_class_distribution.csv", index=False)
-        print_df_post.to_csv("removed_manual_clean_class_distribution.csv", index=False)
+        print_df_clean.to_csv("manual_clean_class_distribution.csv", index=False)
+        print_df_removed.to_csv("removed_data_class_distribution.csv", index=False)
 
 
 
