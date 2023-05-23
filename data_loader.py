bb2a50afe46cdcde2a4438e05e9bfa6536fecee5
Jacob-Link <jklklink@gmail.com>
2023-05-23 22:00:16 +0300

    print removed data manually

--- data_loader.py
+++ data_loader.py
@@ -35,8 +35,48 @@ def print_counter(count_labels_dict, export=False):
         print_df.to_csv("class_distribution.csv", index=False)
 
 
+def print_counter_post_manual_clean(count_labels_dict, export=False):
+    pre_manual_clean = []
+    post_manual_clean = []  # called the new folder example: 1_out
+    total_labels = sum(count_labels_dict.values())
+    for key, val in count_labels_dict.items():
+        tup = (key, val, f"{round(100 * val / total_labels, 2)}%")
+        if "_out" in key:
+            post_manual_clean.append(tup)
+        else:
+            pre_manual_clean.append(tup)
+
+    pre_manual_clean.sort(key=lambda x: x[1], reverse=True)
+    post_manual_clean.sort(key=lambda x: x[1], reverse=True)
+
+    print_df_pre = pd.DataFrame(pre_manual_clean, columns=["class", "count", "%"])
+    print_df_post = pd.DataFrame(post_manual_clean, columns=["class", "count", "%"])
+    print("---------- manual clean ----------")
+    print(print_df_pre)
+    print(f"Size of clean train dataset: {sum(print_df_pre['count']):,}")
+    print("--------------------------------------")
+    print()
+
+    print("---------- removed manual clean ----------")
+    print(print_df_post)
+    print(f"Size of removed: {sum(print_df_post['count']):,}")
+    print("--------------------------------------")
+
+    if export:
+        print_df_pre.to_csv("manual_clean_class_distribution.csv", index=False)
+        print_df_post.to_csv("removed_manual_clean_class_distribution.csv", index=False)
+
+
+
+
 if __name__ == '__main__':
-    count_labels_dict = load_balanced_class_check()
-    print_counter(count_labels_dict)
+    initial_balance_check = False
+    post_manual_clean_check = True
 
+    if initial_balance_check:
+        count_labels_dict = load_balanced_class_check()
+        print_counter(count_labels_dict)
 
+    if post_manual_clean_check:
+        count_labels_dict = load_balanced_class_check()
+        print_counter_post_manual_clean(count_labels_dict)
