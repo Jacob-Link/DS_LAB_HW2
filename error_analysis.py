c7de92aff944e4354a799bb4fd8acbfe47088caa
Jacob-Link <jklklink@gmail.com>
2023-06-03 11:36:50 +0300

    error analysis
    - added missed labelled data extraction from the prediction on validation set.
    - error analysis will export excel with sorted rows by the loss of each single image

---
+++ error_analysis.py
@@ -0,0 +1,5 @@
+from model_loader import get_missed_labels
+
+if __name__ == '__main__':
+    missed = get_missed_labels()
+    print(missed)
--- model_loader.py
+++ model_loader.py
@@ -3,8 +3,11 @@ from data_loader import load_dataset
 import torch.nn as nn
 import torch
 from torchvision import models
+import numpy as np
 
 BATCH_SIZE = 16
+DATA_PATH = Path.cwd().parent / 'online data' / 'test-data-challenge' / 'val'
+MODEL_PATH = Path.cwd().parent / 'models' / "trained_model_8850_0p9_limit_rotation.pt"
 
 
 def init_loader(data):
@@ -21,41 +24,66 @@ def load_state_dict(model, model_path):
 def print_stats(correct, data_size):
     print()
     print("*******************")
-    print(f"Accuracy on the test set supplied: {100*correct.double()/data_size:.4f}%")
+    print(f"Accuracy on the test set supplied: {100 * correct.double() / data_size:.4f}%")
     print("*******************")
 
 
-def predict_data(model, data_loader):
+def predict_data(model, data_loader, device, criterion):
     print("predicting test set...")
     running_corrects = 0
     model.eval()
     with torch.no_grad():
+        missed = dict()  # log all missed classified images for error analysis
         for i, (inputs, classes) in enumerate(data_loader):
             inputs = inputs.to(device)
             classes = classes.to(device)
             outputs = model(inputs)
             _, preds = torch.max(outputs, 1)
             running_corrects += torch.sum(preds == classes)
-            # print(preds)
-            # print(classes)
+
+            single_losses = np.array([criterion(outputs[i], classes[i]) for i in range(len(preds))])
+
+            diff = torch.argwhere(preds != classes)
+            if len(diff) != 0:
+                missed[f'batch_{i}'] = {"img": inputs[diff], "gt": classes[diff], "pred": preds[diff],
+                                        "losses": single_losses[diff]}
+            else:
+                missed[f'batch_{i}'] = "predicted all correctly"
+
     print(">>> finished predicting on test set!")
-    return running_corrects
+    return running_corrects, missed
+
+
+def get_missed_labels():
+    test_data = load_dataset(DATA_PATH)
+    test_dataloader = init_loader(test_data)
+    NUM_CLASSES = len(test_data.classes)
+    model_ft = models.resnet50(pretrained=False)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, NUM_CLASSES)
+    model = load_state_dict(model_ft, MODEL_PATH)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_function = nn.CrossEntropyLoss()
+    _, missed = predict_data(model, test_dataloader, device, loss_function)
+    return missed
 
 
 if __name__ == '__main__':
-    data_path = Path.cwd().parent / 'online data' / 'test-data-challenge' / 'val'
+    # data_path = Path.cwd().parent / 'online data' / 'test-data-challenge' / 'val'
     # model_path = Path.cwd().parent / 'models' / 'trained_model_1.pt'
     # model_path = Path.cwd().parent / 'models' / 'trained_model_2.pt'
     # model_path = Path.cwd().parent / 'models' / "trained_model_10k_black.pt"
-    model_path = Path.cwd().parent / 'models' / "trained_model_10k_white.pt"
+    # model_path = Path.cwd().parent / 'models' / "trained_model_10k_white.pt"
+    # model_path = Path.cwd().parent / 'models' / "trained_model_8850_no_weird_flips.pt"
 
-    test_data = load_dataset(data_path)
+    test_data = load_dataset(DATA_PATH)
     test_dataloader = init_loader(test_data)
 
     class_names = test_data.classes
     print("The classes are: ", class_names)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_function = nn.CrossEntropyLoss()
     NUM_CLASSES = len(class_names)
 
     # used from code given in assignment
@@ -64,6 +92,6 @@ if __name__ == '__main__':
     model_ft.fc = nn.Linear(num_ftrs, NUM_CLASSES)
 
     # load model, predict, print_results
-    model = load_state_dict(model_ft, model_path)
-    correct = predict_data(model_ft, test_dataloader)
+    model = load_state_dict(model_ft, MODEL_PATH)
+    correct, _ = predict_data(model, test_dataloader, device, loss_function)
     print_stats(correct, len(test_data))
